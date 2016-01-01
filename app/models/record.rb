5147d576f3edb92a914d6658a15c4a628a3c0774
katsumata_ryo <ryo@andamp.jp>
2016-01-02 00:26:10 +0900

    テストとヴァリデーションの追加

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,4 +2,5 @@ class Record < ActiveRecord::Base
   # validation
   validates_presence_of :payment, :category
   validates_numericality_of :payment, :greater_than_or_equal_to => 0
+  validates :card, inclusion: { in: [true, false] }
 end
--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe Record, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Record do
+  describe "test" do
+    before do
+      record          = Record.new
+      record.payment  = 0
+      record.date     = Date.today
+      record.category = "eat"
+      record.card     = false
+      record.memo     = "test"
+      record.save
+      @record = Record.first
+    end
+
+    context "normal case." do
+      it "not null" do
+        expect(@record.payment).not_to be_nil
+      end
+
+      it "card is require boolean" do
+        expect(@record.card).to match(true).or match(false)
+        expect(@record.save).to be_truthy
+      end
+    end
+
+    context "error case." do
+      it "is invalid payment value" do
+        [-1000, nil].each do |value|
+          @record.payment = value
+          expect(@record.save).to be_falsey
+        end
+      end
+
+      it "empty category value is invalid" do
+        @record.category = nil
+        expect(@record.save).to be_falsey
+      end
+    end
+  end
 end
