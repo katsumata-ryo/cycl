5064bd50a0e2026f2108b41120d0ca486fff4643
katsumata_ryo <ryo@andamp.jp>
2016-01-02 11:21:28 +0900

    categoryテストの作成

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+describe Category do
+  before do
+    category = Category.new
+    category.name    = "食費"
+    category.monthly = true
+    category.budget  = 10000
+    category.save
+    @category = Category.first
+  end
+
+  context "normal case" do
+    it "can save" do
+      expect(@category.save).to be_truthy
+    end
+
+    it "can save normal name value" do
+      values = ["食費", "eating", "10000"]
+      values.each do |value|
+        @category.name = value
+        expect(@category.save).to be_truthy
+      end
+    end
+
+    it "can save normal monthly value" do
+      values = [true, false]
+      values.each do |value|
+        @category.monthly = value
+        expect(@category.save).to be_truthy
+      end
+    end
+
+    it "can save normal budget value" do
+      values = [0, 1, 10000, 100000000]
+      values.each do |value|
+        @category.budget = value
+        expect(@category.save).to be_truthy
+      end
+    end
+  end
+
+  context "error case" do
+    it "can not save invalid name value" do
+      values = [nil, true, false, ""]
+      values.each do |value|
+        @category.name = value
+        expect(@category.save).to be_falsey
+      end
+    end
+
+    it "can not save invalid monthly value" do
+      values = [nil, 1, "string", ""]
+      values.each do |value|
+        @category.monthly = value
+        expect(@category.save).to be_falsey
+      end
+    end
+
+    it "can not save invalid budget value" do
+      values = [nil, true, "string", ""]
+      values.each do |value|
+        @category.budget = value
+        expect(@category.save).to be_falsey
+      end
+    end
+  end
+end
