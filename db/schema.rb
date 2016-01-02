547f4d5fd9a3c8346505280186d510b6ce7e58e2
katsumata_ryo <ryo@andamp.jp>
2016-01-02 11:21:10 +0900

    do db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151231143929) do
+ActiveRecord::Schema.define(version: 20160102014928) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "monthly"
+    t.integer  "budget"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "installs", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
