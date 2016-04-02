075bdc220f3b253c83a55d8c63fdfbd489a3331d
katsumata_ryo <ktmt.ryo@gmail.com>
2016-04-03 01:13:37 +0900

    cont

---
+++ app/controllers/incomes_controller.rb
@@ -0,0 +1,16 @@
+class IncomesController < ApplicationController
+  def income
+  end
+
+  def date
+  end
+
+  def memo
+  end
+
+  def category_id
+  end
+
+  def user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323154227) do
+ActiveRecord::Schema.define(version: 20160402160240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 20160323154227) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+  end
+
+  create_table "incomes", force: :cascade do |t|
+    t.integer  "income",      null: false
+    t.date     "date",        null: false
+    t.text     "memo"
+    t.integer  "category_id", null: false
+    t.integer  "user_id",     null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "installs", force: :cascade do |t|
