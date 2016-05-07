28a11c20315be02a4c291dbc04f8093a4362ff9a
katsumata_ryo <ktmt.ryo@gmail.com>
2016-05-07 18:52:01 +0900

    モデルのリレーションに関するテストを追加

--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -15,19 +15,42 @@
 
 require 'rails_helper'
 
+# Relations
 describe Record do
-  describe "test" do
-    before do
-      record             = Record.new
-      record.money     = 0
-      record.date        = Date.today
-      record.category_id = 1
-      record.card        = false
-      record.memo        = "test"
-      record.save
-      @record = Record.first
+  fixtures :categories
+  fixtures :users
+
+  before do
+    user = users(:user_01)
+    user.save
+    category = categories(:category_01)
+    category.save
+
+    record             = Record.new
+    record.money       = 0
+    record.date        = Date.today
+    record.category_id = category.id
+    record.user_id     = user.id
+    record.card        = false
+    record.memo        = "test"
+    record.save
+    @record = Record.first
+  end
+
+  describe 'Relations' do
+    it 'belongs_to category' do
+      expect(@record.category).to be_truthy
     end
 
+    it 'belongs to user' do
+      expect(@record.user).to be_truthy
+    end
+  end
+
+  # Validations
+
+  # Methods
+  describe "test" do
     context "normal case." do
       it "not null" do
         expect(@record.money).not_to be_nil
