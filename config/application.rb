0fbc5acc3ec6c03a734b25cc57c2064914d4461a
katsumata_ryo <ktmt.ryo@gmail.com>
2016-05-14 19:27:30 +0900

    SpecのGenerator設定を追加

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,17 @@ module Tucho
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Rspec generator config
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixture: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
   end
 end
