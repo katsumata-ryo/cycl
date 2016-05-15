9a054873e1414d1075f5150d9f4d33293b36f65b
katsumata_ryo <ktmt.ryo@gmail.com>
2016-05-15 12:30:36 +0900

    個別の月ごとに正常にページが取得できるかどうかのテスト

--- spec/requests/records/months_spec.rb
+++ spec/requests/records/months_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe "Records::Months", type: :request do
   describe "GET /records/:years/:months" do
     it "works! (now write some real specs)" do
-      get '/records/2016/5'
-      expect(response).to have_http_status(302)
+      ['2015/1', '2015/12', '2016/1', '2016/5'].each do |date|
+        get "/records/#{date}"
+        expect(response).to have_http_status(302)
+      end
     end
   end
 end
